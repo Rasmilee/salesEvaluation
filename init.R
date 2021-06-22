043d158023e3dbce66cc62a95db1ba3d07e41ed1
Rasmila Lama <rasmila.lamac5@gmail.com>
2021-06-22 21:12:03 +0545

    Update init.R

--- init.R
+++ init.R
@@ -1,5 +1,17 @@
-my_packages = c("randomForest", "data.table")
-
+my_packages = c("randomForest", "data.table","shiny","shinydashboard","tidyverse", "lubridate","dplyr","tidyr","shinyjs","sodium","TTR", "forecast","party")
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+library(tidyr)
+library(shinyjs)
+library(sodium)
+library(data.table)
+library(TTR)
+library(forecast)
+library(party)
+library(randomForest)
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
     install.packages(p)
